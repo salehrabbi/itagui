93a3c4034953e23988047ec1703e1621de8bf4f3
salehrabbi <33930388+salehrabbi@users.noreply.github.com>
2018-02-25 20:34:02 +0600

    service

--- js/custom.js
+++ js/custom.js
@@ -41,9 +41,39 @@
 })(jQuery);
 
 
-$(document).ready(function(){
-  $(".owl-carousel-1").owlCarousel();
-});
+$('.owl-carousel-1').owlCarousel({
+    nav: true,
+    dots: true,
+    autoplay: true,
+    autoplayHoverPause: true,
+    rewind: true,
+    navText: ['<i class="fas fa-angle-left"></i>','<i class="fas fa-angle-right"></i>'],
+    loop:true,
+    margin:10,
+    responsiveClass:true,
+    
+    responsive:{
+        0:{
+            items:2,
+            nav:false,
+            loop: false
+        },
+        480:{
+            items:3,
+            nav:false,
+            loop: false
+        },
+        576:{
+            items:3,
+        },
+        768:{
+            items:4,
+        },
+        992:{
+            items:5,
+        }
+    }
+})
 
 
 
